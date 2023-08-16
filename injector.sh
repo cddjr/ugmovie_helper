0533e9d739353b6eec01e75231c5b4d8df2490db
cddjr <dengjingren@foxmail.com>
2023-08-16 14:24:39 +0800

    Update cmake

--- injector.sh
+++ injector.sh
@@ -11,4 +11,17 @@ if [ ! -f "$lib" ]; then
     exit 1
 fi
 
+cat /proc/$pid/maps | grep -qF "$lib"
+if [ $? -eq 0 ]; then
+  echo "$lib - Already injected" >&2
+  exit 1
+fi
+
+chmod +x "$(dirname "$0")"/linux_injector
 "$(dirname "$0")"/linux_injector 1 $pid "$lib"
+
+cat /proc/$pid/maps | grep -qF "$lib"
+if [ $? -ne 0 ]; then
+  echo "$lib - Operation failed" >&2
+  exit 1
+fi
